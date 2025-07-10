bffa385287efdf90e658dc0ef9d505a677a669d7
jkuya <kuyajesee@proton.me>
2025-07-10 16:51:11 +0300

    refactor: return only the fileInfo and error

--- util/isValidDir.go
+++ util/isValidDir.go
@@ -6,11 +6,11 @@ import (
 )
 
 // IsValidDir ensures dirPath is a valid directory
-func IsValidDir(dirPath string) (bool, os.FileInfo, error) {
+func IsValidDir(dirPath string) (os.FileInfo, error) {
 	info, err := os.Stat(dirPath)
 	if err != nil {
-		return false, nil, fmt.Errorf("cannot access '%v': No such file or directory", dirPath)
+		return nil, fmt.Errorf("cannot access '%v': No such file or directory", dirPath)
 	}
 
-	return true, info, nil
+	return info, nil
 }
