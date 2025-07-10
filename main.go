b9168650dba650b739993fee28e99c37a0cd542d
vomolo <valentineomolo1@gmail.com>
2025-07-10 23:12:58 +0300

    fix: pass ShowHidden flag to ReadDirNames for correct file visibility

--- main.go
+++ main.go
@@ -2,7 +2,6 @@ package main
 
 import (
 	"os"
-	"strings"
 
 	"github.com/jesee-kuya/my-ls/print"
 )
@@ -21,33 +20,6 @@ func main() {
 
 func ParseArgs(args []string) (print.Flags, []string) {
 	var flags print.Flags
-	var paths []string
-
-	for _, arg := range args {
-		if strings.HasPrefix(arg, "-") {
-			// Handle flags - loop through each character after the dash
-			for _, char := range arg[1:] {
-				switch char {
-				case 'a':
-					flags.ShowHidden = true
-				}
-			}
-		} else {
-			// It's a path
-			paths = append(paths, arg)
-		}
-	}
-
-	// Default to current directory if no paths specified
-	if len(paths) == 0 {
-		paths = []string{"."}
-	}
-
-	return flags, paths
-}
-
-func parseArgs(args []string) (Flags, []string) {
-	var flags Flags
 	var paths []string
 
 	for _, arg := range args {
