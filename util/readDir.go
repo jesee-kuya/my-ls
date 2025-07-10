5fb12e6ea8b9e6d05675cc0e8e23b15e56ae79f9
jkuya <kuyajesee@proton.me>
2025-07-10 22:07:33 +0300

    refactor: add all color codes followed by ls function

--- util/readDir.go
+++ util/readDir.go
@@ -3,6 +3,19 @@ package util
 import (
 	"fmt"
 	"os"
+	"strings"
+)
+
+const (
+	reset = "\033[0m"
+
+	dirColour     = "\033[01;34m"    // bold blue
+	exeColour     = "\033[01;32m"    // bold green
+	symlinkColour = "\033[01;36m"    // bold cyan
+	socketColour  = "\033[01;35m"    // bold magenta
+	pipeColour    = "\033[40;33m"    // yellow on black background
+	deviceColour  = "\033[40;33;01m" // bold yellow on black (block/char dev)
+	archiveColour = "\033[01;31m"    // bold red
 )
 
 // ReadDirNames returns a list of file and directory names in dirPath
@@ -20,11 +33,39 @@ func ReadDirNames(dirPath string) ([]string, error) {
 
 	var names []string
 	for _, entry := range entries {
-		if entry.IsDir() {
-			names = append(names, fmt.Sprintf("%v%v", "\x1b[34m", entry.Name()))
-		} else {
-			names = append(names, fmt.Sprintf("%v%v", "\x1b[0m", entry.Name()))
+		name := entry.Name()
+		mode := entry.Mode()
+		colour := reset
+
+		switch {
+		case mode.IsDir():
+			colour = dirColour
+
+		case mode&os.ModeSymlink != 0:
+			colour = symlinkColour
+
+		case mode&os.ModeSocket != 0:
+			colour = socketColour
+
+		case mode&os.ModeNamedPipe != 0:
+			colour = pipeColour
+
+		case mode&os.ModeDevice != 0:
+			colour = deviceColour
+
+		case mode&0o111 != 0:
+			colour = exeColour
+
+		case strings.HasSuffix(name, ".tar") ||
+			strings.HasSuffix(name, ".gz") ||
+			strings.HasSuffix(name, ".tgz") ||
+			strings.HasSuffix(name, ".zip") ||
+			strings.HasSuffix(name, ".bz2") ||
+			strings.HasSuffix(name, ".xz"):
+			colour = archiveColour
 		}
+
+		names = append(names, fmt.Sprintf("%s%s%s", colour, name, reset))
 	}
 
 	return names, nil
