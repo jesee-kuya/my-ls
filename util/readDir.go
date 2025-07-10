99b85751be81b5c6bb4fc73c864da6d845e3a361
jkuya <kuyajesee@proton.me>
2025-07-10 16:04:25 +0300

    feat: write a function that returns a list of file and directory names in a specific path

---
+++ util/readDir.go
@@ -0,0 +1,29 @@
+package util
+
+import (
+	"errors"
+	"os"
+)
+
+// ReadDirNames returns a list of file and directory names in dirPath
+func ReadDirNames(dirPath string) ([]string, error) {
+	dir, err := os.Open(dirPath)
+	if err != nil {
+		return nil, err
+	}
+	defer dir.Close()
+
+	entries, err := dir.Readdir(-1)
+	if err != nil {
+		return nil, err
+	}
+
+	var names []string
+	for _, entry := range entries {
+		names = append(names, entry.Name())
+	}
+	if names == nil {
+		return nil, errors.New("no entries found")
+	}
+	return names, nil
+}
