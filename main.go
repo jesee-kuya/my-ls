f1d05edab2ce4ee97131dc534575482264146e82
vomolo <valentineomolo1@gmail.com>
2025-07-11 19:31:47 +0300

    feat: implement basic flag parsing logic for -a flag

--- main.go
+++ main.go
@@ -16,7 +16,25 @@ func parseArgs(args []string) (Flags, []string) {
 	flags := Flags{}
 	var paths []string
 
-	// TODO: implement flag parsing logic
+	for _, arg := range args {
+		if len(arg) > 0 && arg[0] == '-' {
+			// Parse flags
+			for _, char := range arg[1:] {
+				switch char {
+				case 'a':
+					flags.ShowAll = true
+				}
+			}
+		} else {
+			// It's a path
+			paths = append(paths, arg)
+		}
+	}
+
+	// If no paths specified, use current directory
+	if len(paths) == 0 {
+		paths = []string{"."}
+	}
 
 	return flags, paths
 }
