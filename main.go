c6b628d90aa47bf99664acb316cd6ed903c39eea
vomolo <valentineomolo1@gmail.com>
2025-07-11 19:29:53 +0300

    feat: add parseArgs function skeleton

--- main.go
+++ main.go
@@ -11,6 +11,16 @@ type Flags struct {
 	ShowAll bool // -a flag: show all files including hidden ones
 }
 
+// parseArgs parses command-line arguments and returns flags and paths
+func parseArgs(args []string) (Flags, []string) {
+	flags := Flags{}
+	var paths []string
+
+	// TODO: implement flag parsing logic
+
+	return flags, paths
+}
+
 func main() {
 	paths := []string{"."}
 
