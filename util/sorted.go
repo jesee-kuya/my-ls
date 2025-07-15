54e60f770d7ca73095b592f74a993eca20976059
vomolo <valentineomolo1@gmail.com>
2025-07-15 12:22:58 +0300

    Add time-based sorting functions
    - Added InsertSortedByTime function to sort files by modification time (newest first)
    - Added InsertSortedLongByTime function for long format time-based sorting
    - Both functions fall back to alphabetical sorting if time info is unavailable
    - Added necessary imports (os, time) for file stat operations

--- util/sorted.go
+++ util/sorted.go
@@ -2,6 +2,7 @@ package util
 
 import (
 	"fmt"
+	"os"
 	"strings"
 )
 
@@ -32,4 +33,78 @@ func InsertSortedLong(line string, lines []string) []string {
 
 func TrimStart(name string) string {
 	return strings.TrimLeft(name, ".")
+}
+
+// InsertSortedByTime inserts a file into a list sorted by modification time (newest first)
+func InsertSortedByTime(name, colour, reset, dirPath string, names []string) []string {
+	if name == "." || name == ".." {
+		return append([]string{fmt.Sprintf("%s%s%s", colour, name, reset)}, names...)
+	}
+	colored := fmt.Sprintf("%s%s%s", colour, name, reset)
+
+	// Get modification time for the new file
+	newFilePath := joinPath(dirPath, name)
+	newInfo, err := os.Stat(newFilePath)
+	if err != nil {
+		// If we can't get the time, fall back to alphabetical sorting
+		return InsertSorted(name, colour, reset, names)
+	}
+	newTime := newInfo.ModTime()
+
+	for i, val := range names {
+		cleanVal := StripANSI(val)
+		if cleanVal == "." || cleanVal == ".." {
+			continue
+		}
+
+		// Get modification time for the existing file
+		existingFilePath := joinPath(dirPath, cleanVal)
+		existingInfo, err := os.Stat(existingFilePath)
+		if err != nil {
+			// If we can't get the time, insert here
+			return append(names[:i], append([]string{colored}, names[i:]...)...)
+		}
+		existingTime := existingInfo.ModTime()
+
+		// Insert if new file is newer (or same time, then alphabetical)
+		if newTime.After(existingTime) || (newTime.Equal(existingTime) && strings.ToLower(name) <= strings.ToLower(cleanVal)) {
+			return append(names[:i], append([]string{colored}, names[i:]...)...)
+		}
+	}
+
+	return append(names, colored)
+}
+
+// InsertSortedLongByTime inserts a long format line sorted by modification time (newest first)
+func InsertSortedLongByTime(line, dirPath string, lines []string) []string {
+	fileName := StripANSI(StripLong(line))
+
+	// Get modification time for the new file
+	newFilePath := joinPath(dirPath, fileName)
+	newInfo, err := os.Stat(newFilePath)
+	if err != nil {
+		// If we can't get the time, fall back to alphabetical sorting
+		return InsertSortedLong(line, lines)
+	}
+	newTime := newInfo.ModTime()
+
+	for i, val := range lines {
+		existingFileName := StripANSI(StripLong(val))
+
+		// Get modification time for the existing file
+		existingFilePath := joinPath(dirPath, existingFileName)
+		existingInfo, err := os.Stat(existingFilePath)
+		if err != nil {
+			// If we can't get the time, insert here
+			return append(lines[:i], append([]string{line}, lines[i:]...)...)
+		}
+		existingTime := existingInfo.ModTime()
+
+		// Insert if new file is newer (or same time, then alphabetical)
+		if newTime.After(existingTime) || (newTime.Equal(existingTime) && strings.ToLower(fileName) <= strings.ToLower(existingFileName)) {
+			return append(lines[:i], append([]string{line}, lines[i:]...)...)
+		}
+	}
+
+	return append(lines, line)
 }
